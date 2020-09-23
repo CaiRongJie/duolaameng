252b0665c63889902875cfb61fe23cb06f630285
Administrator <Administrator@WIN-SQ0IF5BAC2U>
2020-09-23 10:33:51 +0800

    Add an index page

--- index.php
+++ index.php
@@ -11,7 +11,44 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "123456789";
+         include('Student.php');
+         $students = array();
+           
+$first = new Student();
+$first->surname = "Doe";
+$first->first_name = "John";
+$first->add_status('status','Freshman');
+$first->add_email('home','john@doe.com');
+$first->add_email('work','jdoe@mcdonalds.com');
+$first->add_grade(65);
+$first->add_grade(75);
+$first->add_grade(55);
+$students['j123'] = $first;
+
+$second = new Student();
+$second->surname = "Einstein";
+$second->first_name = "Albert";
+$second->add_status('status','Sophomore');
+$second->add_email('home','albert@braniacs.com');
+$second->add_email('work1','a_einstein@bcit.ca');
+$second->add_email('work2','albert@physics.mit.edu');
+$second->add_grade(95);
+$second->add_grade(80);
+$second->add_grade(50);
+$students['a456'] = $second;
+
+$a3 = new Student();
+$a3->surname = "Cai";
+$a3->first_name = "RongJie";
+$a3->add_status('status','Sophomore');
+$a3->add_email('work','RongJie@bcit.com');
+$a3->add_grade(100);
+$a3->add_grade(100);
+$a3->add_grade(100);
+$students['r123'] = $a3;
+        foreach($students as $student)
+        echo $student->toString();
         ?>
+       
     </body>
 </html>
